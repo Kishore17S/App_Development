4119023c626234271e043e60260654b3ef7660d0
Kishore17S <144918482+Kishore17S@users.noreply.github.com>
2023-10-16 02:21:33 +0530

    Delete Day 21 directory

--- Day 21/src/main/java/com/ecommerce/repository/Exerciserepo.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.entity.Exercise;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Exerciserepo extends JpaRepository<Exercise, Integer> {
-}
